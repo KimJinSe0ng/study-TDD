1dcb889933afa1a6c6c6f4cc01389d2e45f4a19a
KimJinSe0ng <waujoxc@gmail.com>
2024-01-07 17:55:49 +0900

    feat(ch03.sec01): 초반에 복잡한 테스트부터 시작하면 안 되는 이유

---
+++ tdd-start/src/test/java/study/tddstart/ch03/sec01/PasswordStrengthMeter.java
@@ -0,0 +1,9 @@
+package study.tddstart.ch03.sec01;
+
+public class PasswordStrengthMeter {
+
+    public PasswordStrength meter(String s) {
+        if ("abcDef12".equals(s)) return PasswordStrength.STRONG; //모든 규칙을 충족하는 경우 빨리 통과시키는 방법
+        return PasswordStrength.WEAK;
+    }
+}
---
+++ tdd-start/src/test/java/study/tddstart/ch03/sec01/PasswordStrengthMeterTest.java
@@ -0,0 +1,32 @@
+package study.tddstart.ch03.sec01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * TDD 예: 암호 검사기
+ * 암호 검사기는 문자열을 검사해 규칙을 준수하는지에 따라 암호를 '약함, 보통, 강함'으로 구분한다.
+ * 검사할 규칙은 다음과 같다.
+ * 1. 길이가 8글자 이상
+ * 2. 0부터 9사이의 숫자를 포함
+ * 3. 대문자 포함
+ * 세 규칙을 모두 충족하면 '강함', 2개의 규칙을 충족하면 '보통', 1개 이하의 규칙을 충족하면 '약함'
+ */
+public class PasswordStrengthMeterTest { //1.테스트할 기능의 이름 정하기
+    //초반에 복잡한 테스트부터 시작하면 안 되는 이유
+    //1.대문자 포함 규칙만 충족하는 경우
+    @Test
+    void meetsOnlyUpperCreteria_Then_Weak() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("abcDef");
+        assertEquals(PasswordStrength.WEAK, result);
+    }
+
+    //2.모든 규칙을 충족하는 경우
+    void meetsAllCreteria_Then_Weak() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("abcDef12");
+        assertEquals(PasswordStrength.STRONG, result);
+    }
+}
