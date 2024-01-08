82801678887452dee6a3902ed37556c1e75d5470
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 15:42:20 +0900

    refactor(ch03.sec02): 첫 납부일 존재 여부에 따라 가독성 개선

--- tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
+++ tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
@@ -7,17 +7,23 @@ public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); //후보 만료일 구함
-            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
-            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) { //첫 납부일의 일자와 후보 만료일의 일자가 다르면
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < dayOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon); //이 조건이 참이면 후보 만료일을 그달의 마지막 날로 조정
-                }
-                return candidateExp.withDayOfMonth(dayOfFirstBilling); //첫 납부일의 일자를 후보 만료일의 일자로 사용 + 4월 31일이 없는데 31일로 설정해서 예외 발생
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
+    }
 
-        return payData.getBillingDate().plusMonths(addedMonths);
+    private static LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); //후보 만료일 구함
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) { //첫 납부일의 일자와 후보 만료일의 일자가 다르면
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
     }
 }
