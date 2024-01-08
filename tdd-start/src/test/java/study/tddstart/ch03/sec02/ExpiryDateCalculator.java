6b7a46949b3fcdfce698d3d1c3c3b90042727023
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 15:38:52 +0900

    refactor(ch03.sec02): 첫 납부일의 일자를 구하는 코드 중복 제거

--- tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
+++ tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
@@ -8,12 +8,13 @@ public class ExpiryDateCalculator {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); //후보 만료일 구함
-            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) { //첫 납부일의 일자와 후보 만료일의 일자가 다르면
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) { //첫 납부일의 일자와 후보 만료일의 일자가 다르면
                 final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                if (dayLenOfCandiMon < dayOfFirstBilling) {
                     return candidateExp.withDayOfMonth(dayLenOfCandiMon); //이 조건이 참이면 후보 만료일을 그달의 마지막 날로 조정
                 }
-                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); //첫 납부일의 일자를 후보 만료일의 일자로 사용 + 4월 31일이 없는데 31일로 설정해서 예외 발생
+                return candidateExp.withDayOfMonth(dayOfFirstBilling); //첫 납부일의 일자를 후보 만료일의 일자로 사용 + 4월 31일이 없는데 31일로 설정해서 예외 발생
             }
         }
 
