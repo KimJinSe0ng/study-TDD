2cb28b557b0b1f250a155a24cd541ae439b99f65
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 14:16:40 +0900

    feat(ch03.sec02): 쉬운 것부터 테스트

---
+++ tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
@@ -0,0 +1,9 @@
+package study.tddstart.ch03.sec02;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019, 4, 1);
+    }
+}
