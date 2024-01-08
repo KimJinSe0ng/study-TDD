b1f120f9e4e8452964b3989f27b0da13d77d937b
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 15:37:03 +0900

    refactor(ch03.sec02): 후보 만료일이 속한 월의 마지막 일자를 구하는 코드의 중복 제거

--- tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
+++ tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
@@ -9,8 +9,9 @@ public class ExpiryDateCalculator {
         if (payData.getFirstBillingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); //후보 만료일 구함
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) { //첫 납부일의 일자와 후보 만료일의 일자가 다르면
-                if (YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth()); //이 조건이 참이면 후보 만료일을 그달의 마지막 날로 조정
+                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+                if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(dayLenOfCandiMon); //이 조건이 참이면 후보 만료일을 그달의 마지막 날로 조정
                 }
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); //첫 납부일의 일자를 후보 만료일의 일자로 사용 + 4월 31일이 없는데 31일로 설정해서 예외 발생
             }
