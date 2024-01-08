1f94b5fff93169485c349164a7b64ca5958d5665
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 15:19:46 +0900

    feat(ch03.sec02): 예외 상황 테스트 진행 계속 - 일반화

--- tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
+++ tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
@@ -5,9 +5,13 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         if (payData.getFirstBillingDate() != null) {
-            if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-                return LocalDate.of(2019, 3, 31);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(1); //후보 만료일 구함
+            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) { //첫 납부일의 일자와 후보 만료일의 일자가 다르면
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); //첫 납부일의 일자를 후보 만료일의 일자로 사용
             }
+//            if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
+//                return LocalDate.of(2019, 3, 31);
+//            }
         }
 
         return payData.getBillingDate().plusMonths(1);
