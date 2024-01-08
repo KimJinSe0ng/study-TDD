430cb0c19c0dd8d0f3647cfb0486e47ddde9b843
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 15:00:49 +0900

    feat(ch03.sec02): 다음 테스트를 추가하기 전에 리팩터링

--- tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
+++ tdd-start/src/test/java/study/tddstart/ch03/sec02/ExpiryDateCalculator.java
@@ -3,7 +3,7 @@ package study.tddstart.ch03.sec02;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+    public LocalDate calculateExpiryDate(PayData payData) {
+        return payData.getBillingDate().plusMonths(1);
     }
 }
