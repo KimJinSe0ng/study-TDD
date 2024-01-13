c9373bae19cb7197973d84c683cceb4d5b642b4d
KimJinSe0ng <waujoxc@gmail.com>
2024-01-13 20:52:32 +0900

    feat(appC): 인자 매칭 처리 - 주의 사항

--- tdd-start/src/test/java/study/tddstart/appC/AnyMatcherTest.java
+++ tdd-start/src/test/java/study/tddstart/appC/AnyMatcherTest.java
@@ -24,7 +24,16 @@ public class AnyMatcherTest { //인자 매칭 처리
     }
 
     @Test
+    void 스텁을_설정할_메서드의_인자가_두_개_이상인_경우() {
+        //Mockito는 한 인자라도 ArgumentMatchers를 사용해서 설정한 경우 모든 인자를 ArgumentMatchers를 이용해서 설정하도록 하고 있다.
+        List<String> mockList = mock(List.class);
+        given(mockList.set(anyInt(), "123")).willReturn("456");
+        String old = mockList.set(5, "123");
+    }
+
+    @Test
     void mixAnyAndEq() {
+        //임의의 값과 일치하는 인자와 정확하게 일치하는 인자를 함께 사용하고 싶다면 ArgumentMatchers.eq()를 사용해야 한다.
         List<String> mockList = mock(List.class);
 
         given(mockList.set(anyInt(), eq("123"))).willReturn("456");
