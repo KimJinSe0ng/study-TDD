0d30c9239491256d6fe2c76bf8c976536cd2cd62
KimJinSe0ng <waujoxc@gmail.com>
2024-01-13 20:00:28 +0900

    feat(appA): 조건에 따른 테스트 - 특정 운영체제에 따라 테스트

---
+++ tdd-start/src/test/java/study/tddstart/appA/OsTmpPathTest.java
@@ -0,0 +1,27 @@
+package study.tddstart.appA;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OsTmpPathTest { //조건에 따른 테스트
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void windowTmpPath() {
+        Path tmpPath = Paths.get("c:\\Temp");
+        assertTrue(Files.isDirectory(tmpPath));
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void linuxTmpPath() {
+        Path tmpPath = Paths.get("/tmp");
+        assertTrue(Files.isDirectory(tmpPath));
+    }
+}
